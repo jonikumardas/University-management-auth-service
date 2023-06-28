fedaedf896c2372c51c73577c7d98213b9585c72
jonikumardas <jonikumardas213@gmail.com>
2023-06-29 04:51:59 +0600

    setup has done

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
-import express, { Application, NextFunction, Request, Response } from 'express'
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
-
 // medilewere
 app.use(cors())
 // perser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.get('/', (req: Request, res: any, next: NextFunction) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
-})
+}),
+  app.get('/user', (req: Request, res: Response) => {
+    res.send('hello joni kumar das')
+  })
+
 export default app
